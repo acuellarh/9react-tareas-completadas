b46ad50f2564b3bde1ac78fee4e625d20fcbd580
Alejandro <acuellarh@gmail.com>
2020-04-04 17:18:36 -0500

    Add task feature

--- src/App.js
+++ src/App.js
@@ -14,16 +14,38 @@ class App extends Component {
       newTask: ''
     }
   }
+
+  
+  addTask(event) {
+    event.preventDefault()
+    const objID = this.state.tasks.length + 1
+    let oldTasks = this.state.tasks
+    let newTask = this.state.newTask
+
+    if (newTask.length !== 0) {
+      this.setState({
+        tasks: [...oldTasks, {id: objID, name: newTask, done: false} ],
+        newTask: ''
+      })
+    }    
+  }
+  
+  updateTask(event) {
+    this.setState({
+      newTask: event.target.value   
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}      
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={this.addTask.bind(this)}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.updateTask.bind(this)} />
           </form>
         </div>
       </div>
