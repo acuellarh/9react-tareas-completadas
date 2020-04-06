c0af0b0de67a264f99ddcfb9300fc2c5db25ba68
Alejandro <acuellarh@gmail.com>
2020-04-05 19:54:12 -0500

    makDone feature

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends Component {
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      inputStyle: ''
     }
   }
 
@@ -27,6 +28,10 @@ class App extends Component {
         tasks: [...oldTasks, {id: objID, name: newTask, done: false} ],
         newTask: ''
       })
+    } else {
+      this.setState({
+        inputStyle: 'error'        
+      })
     }    
   }
   
@@ -36,16 +41,25 @@ class App extends Component {
     })
   }
 
+  markDone(e) {
+    const idNum = e.target.value
+    this.setState({
+      tasks: this.state.tasks.map(task =>
+        idNum === task.id ? task.done ? { id: task.id, name: task.name, done: false } : { id: task.id, name: task.name, done: true } : task
+      )
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}      
+            {this.state.tasks.map((task, index) => <li className={ task.done ? 'done' : ''} onClick={this.markDone.bind(this)} key={task.id} value={task.id}>{task.name} </li>) }      
           </ul>
           <form onSubmit={this.addTask.bind(this)}>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} onChange={this.updateTask.bind(this)} />
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" className={this.state.newTask.length === 0 ? this.state.inputStyle : ''} value={this.state.newTask} onChange={this.updateTask.bind(this)} />
           </form>
         </div>
       </div>
